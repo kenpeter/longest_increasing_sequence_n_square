bc11e5b2daf4f24eb99333ee5cbdc8a725a65938
kenpeter <kenpeter@kenpeter>
2018-08-01 12:25:11 +0000

    able to print

--- lis.js
+++ lis.js
@@ -11,15 +11,24 @@ function lis(a) {
 
 	// init
 	var dp = Array(num).fill(1);
+	// key as values
+	var indexArr = [...Array(num).keys()];
 
-	//
+	// loop last item
 	for(var i=1; i<=num; i++) {
-	
+
+		// start 0, down	
 		for(var j=0; j<i; j++) {
+			// prev item < later item	
 			if(a[j] < a[i]) {
+				// prev item + 1 
 				var item = dp[j] + 1;
+				// prev item, more, replace 
 				if(item > dp[i]) {
-					dp[i] = item;	
+					dp[i] = item;
+
+					// prev parent
+					indexArr[i] = j;	
 				} else {
 					// nothing 
 				}	
@@ -33,4 +42,32 @@ function lis(a) {
 	}
 
 	console.log(dp);
+	console.log(indexArr);
+
+	getCombo(a, indexArr);	
 }
+
+
+function combo(i, origArr, indexArr) {
+	var index = indexArr[i];
+
+	//
+	console.log(origArr[i]);
+
+	if(index == i) {
+		return;
+	} else {
+		combo(index, origArr, indexArr);
+	}
+}
+
+function getCombo(origArr, indexArr) {
+	var n = indexArr.length;
+	for(var i=n-1; i>0; i--) {
+		console.log('-- start --');
+		combo(i, origArr, indexArr);	
+	}
+}
+
+
+
